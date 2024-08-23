4db8b377c19527ce00ba9a6a1cf4eede7e77ba2e
Paolo Z <zambetti.paolo@gmail.com>
2024-08-23 17:08:05 +0200

    Initial commit

---
+++ EasyGANTT.js
@@ -0,0 +1,558 @@
+var projectDelay = []
+var data = [
+    {
+        "name": "pulizia casa",
+        "delivery": "2024-10-25",
+        "penalty": "1",
+        "phases": [
+            {
+                "name": "Lavaggio piatti",
+                "duration": "1",
+                "start": "0",
+                "resources": [
+                    {
+                        "name": "Paolo",
+                        "employed": "100"
+                    },
+                    {
+                        "name": "Carlo",
+                        "employed": "50"
+                    }
+                ]
+            },
+            {
+                "name": "Lavaggio pavimenti",
+                "duration": "1",
+                "start": "1",
+                "resources": [
+                    {
+                        "name": "Giorgio",
+                        "employed": "100"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "studio",
+        "delivery": "2024-12-15",
+        "penalty": "10",
+        "phases": [
+            {
+                "name": "matematica",
+                "duration": "1",
+                "start": "1",
+                "resources": [
+                    {
+                        "name": "Carlo",
+                        "employed": "50"
+                    }
+                ]
+            }
+        ]
+    }
+];
+
+var selectedProjectIndex = 0;
+var selectedPhaseIndex = 0;
+var selectedResourceIndex = 0;
+
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
+    if (file && file.type === 'application/json') {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            try {
+                data = JSON.parse(e.target.result);
+                populateProjectsTable();
+            } catch (err) {
+                alert("Errore nel parsing del file JSON.");
+            }
+        };
+        reader.readAsText(file);
+    } else {
+        alert("Per favore, carica un file JSON valido.");
+    }
+}
+
+function downloadJSON() {
+    const json = JSON.stringify(data, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'tabella_dati.json';
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+
+function populateProjectsTable() {
+    var projectsTableBody = document.querySelector('#projectsTable tbody');
+    projectsTableBody.innerHTML = '';
+    console.log(data);
+    data.forEach((project, index) => {
+        var projectRow = document.createElement('tr');
+        projectRow.innerHTML = `
+            <td><input type="text" value="${project.name}" name="name" style="width: 18em" maxlength="30" onchange="updateProjectField(${index}, 'name', this.value)"></td>
+            <td><input type="date" value="${project.delivery}" name="delivery" onchange="updateProjectField(${index}, 'delivery', this.value)"></td>
+            <td><input type="number" value="${project.penalty}" name="penalty"  min="0" max="10" step="1" onchange="updateProjectField(${index}, 'penalty', this.value)"></td>
+        `;
+        if (selectedProjectIndex === index) {
+            projectRow.classList.add('selected1');
+        }
+        projectRow.classList.add("rowBorder");
+        projectRow.addEventListener('click', function() {
+          toggleProjectSelection(index);
+        });
+        projectsTableBody.appendChild(projectRow);
+    });
+    populatePhasesTable();
+}
+
+function populatePhasesTable() {
+    var phasesTableBody = document.querySelector('#phasesTable tbody');
+    phasesTableBody.innerHTML = '';
+
+    var project = data[selectedProjectIndex];
+    project.phases.forEach((phase, index) => {
+        var phaseRow = document.createElement('tr');
+        phaseRow.innerHTML = `
+            <td><input type="text" name="description" value="${phase.name}" style="width: 18em" maxlength="30" onchange="updatePhaseField( ${index}, 'name', this.value)"></td>
+
+            <td><input type="number"  name="start" value="${phase.start}" min="0" max="1" step="1" onchange="updatePhaseField( ${index}, 'start', this.value)"></td>
+
+            <td><input type="number"  name="duration" value="${phase.duration}" min="1" max="1000" step="1" onchange="updatePhaseField( ${index}, 'duration', this.value)"></td>
+
+        `;
+        if (selectedPhaseIndex === index) {
+            phaseRow.classList.add('selected2');
+        }
+        phaseRow.classList.add("rowBorder");
+        phaseRow.addEventListener('click', function() { togglePhaseSelection(index); });
+        phasesTableBody.appendChild(phaseRow);
+        
+    });
+    populateResourcesTable();
+}
+
+function populateResourcesTable() {
+    var resourcesTableBody = document.querySelector('#resourcesTable tbody');
+    resourcesTableBody.innerHTML = '';
+    
+    var phase = data[selectedProjectIndex].phases[selectedPhaseIndex];
+    phase.resources.forEach((resource, index) => {
+        var resourceRow = document.createElement('tr');
+        resourceRow.innerHTML = `
+                <td><input type="text" style="width: 10em;" maxlength="15" value="${resource.name}" onchange="updateResourceField( ${index}, 'name', this.value)"></td>
+                <td><input type="number" value="${resource.employed}" min="1" max="100" step="1" onchange="updateResourceField( ${index}, 'employed', this.value)"></td> 
+            `;    
+        
+        if (selectedResourceIndex === index) {
+            resourceRow.classList.add('selected3');
+        }   
+        resourceRow.classList.add("rowBorder");
+        resourceRow.addEventListener('click', function() {toggleResourceSelection(index);});
+        resourcesTableBody.appendChild(resourceRow);
+    });
+
+}
+
+// Funzione per selezionare un progetto
+function toggleProjectSelection(index) {
+    
+    selectedProjectIndex = index;
+    selectedPhaseIndex = 0; // Resetta l'indice della fase selezionata
+    selectedResourceIndex = 0;
+    // Evidenzia la riga selezionata
+    var rows = document.querySelectorAll('#projectsTable tbody tr');
+    rows.forEach((row, i) => {
+        if (i === index) {
+            row.classList.add('selected1');
+        } else {
+            row.classList.remove('selected1');
+        }
+    });
+
+    // Popola la tabella delle fasi per il progetto selezionato
+    populatePhasesTable();
+}
+
+// Funzione per selezionare una fase
+function togglePhaseSelection(index) {
+    selectedPhaseIndex = index;
+    selectedResourceIndex = 0;
+
+    // Evidenzia la riga selezionata
+    var rows = document.querySelectorAll('#phasesTable tbody tr');
+    rows.forEach((row, i) => {
+        if (i === index) {
+            row.classList.add('selected2');
+        } else {
+            row.classList.remove('selected2');
+        }
+    });
+    populateResourcesTable();
+
+}
+
+function toggleResourceSelection(index) {
+    selectedResourceIndex = index;
+
+    // Evidenzia la riga selezionata
+    var rows = document.querySelectorAll('#resourcesTable tbody tr');
+    rows.forEach((row, i) => {
+        if (i === index) {
+            row.classList.add('selected3');
+        } else {
+            row.classList.remove('selected3');
+        }
+    });
+  
+
+}
+function newProject(){
+    selectedProjectIndex++;
+    selectedPhaseIndex = 0;
+    selectedResourceIndex = 0;
+    data.splice( selectedProjectIndex,0,{
+        "name": "new",
+        "delivery": "2000-1-1",
+        "penalty": 0,
+        "phases": [
+            {
+                "name": "New",
+                "duration": 1,
+                "start": 0,
+                "resources": [
+                    {
+                        "name": "New",
+                        "employed": 100
+                    }
+                ]
+            }
+        ]
+    })
+    populateProjectsTable();
+}
+function newPhase(){
+    selectedPhaseIndex++;
+    selectedResourceIndex = 0;
+    data[selectedProjectIndex].phases.splice( selectedPhaseIndex,0, 
+         
+            {
+                "name": "New",
+                "duration": 1,
+                "start": 0,
+                "resources": [
+                    {
+                        "name": "New",
+                        "employed": 100
+                    }
+                ]
+            }
+    
+    )
+    populatePhasesTable();
+}
+function newResorce(){
+    selectedResourceIndex++;
+    data[selectedProjectIndex].phases[selectedPhaseIndex].resources.splice( selectedResourceIndex,0, 
+                    {
+                        "name": "New",
+                        "employed": 100
+                    }
+    
+    )
+    populateResourcesTable();
+}
+
+function deleteProject(){
+    if (data.length > 1 && confirm("You want to delete the project?")){
+        data.splice( selectedProjectIndex,1);
+        if (data.length === selectedProjectIndex){
+            selectedProjectIndex--;
+        }
+        populateProjectsTable();
+        
+    } 
+
+}
+
+function deletePhase(){
+    if (data[selectedProjectIndex].phases.length > 1 && confirm("You want to delete the phase?")){
+        data[selectedProjectIndex].phases.splice( selectedPhaseIndex,1);
+        if (data[selectedProjectIndex].phases.length === selectedPhaseIndex){
+            selectedPhaseIndex--;  
+        }
+        populatePhasesTable(); 
+    }
+}
+
+function deleteResource(){
+    if(data[selectedProjectIndex].phases[selectedPhaseIndex].resources.length > 1 && confirm("You want to delete the resource?")){
+    data[selectedProjectIndex].phases[selectedPhaseIndex].resources.splice( selectedResourceIndex,1);
+    if(data[selectedProjectIndex].phases[selectedPhaseIndex].resources.length ===  selectedResourceIndex){
+        selectedResourceIndex--;       
+    }
+
+    populateResourcesTable();
+ }
+}
+
+function upProject(){
+    if (data.length > 1 && selectedProjectIndex > 0){
+        selectedData = data[selectedProjectIndex];
+        data.splice( selectedProjectIndex,1);
+        selectedProjectIndex--;
+        data.splice( selectedProjectIndex,0,selectedData);
+        populateProjectsTable();
+    } 
+}
+
+function upPhase(){
+    if (data[selectedProjectIndex].phases.length  > 1 && selectedPhaseIndex > 0){
+        selectedData = data[selectedProjectIndex].phases[selectedPhaseIndex];
+        data[selectedProjectIndex].phases.splice( selectedPhaseIndex,1);
+        selectedPhaseIndex--;
+        data[selectedProjectIndex].phases.splice( selectedPhaseIndex,0,selectedData);
+        if (selectedPhaseIndex === 0 ){
+            data[selectedProjectIndex].phases[selectedPhaseIndex]['start'] = 0;
+        }
+        populateProjectsTable();  
+    } 
+}
+function downProject(){
+    if (data.length > 1 && selectedProjectIndex < data.length-1){
+        selectedData = data[selectedProjectIndex];
+        data.splice( selectedProjectIndex,1);
+        selectedProjectIndex++;
+        data.splice( selectedProjectIndex,0,selectedData);
+        populateProjectsTable();
+    } 
+}
+
+function downPhase(){
+    if (data[selectedProjectIndex].phases.length  > 1 && selectedPhaseIndex < data[selectedProjectIndex].phases.length-1){
+        selectedData = data[selectedProjectIndex].phases[selectedPhaseIndex];
+        data[selectedProjectIndex].phases.splice( selectedPhaseIndex,1);
+        selectedPhaseIndex++;
+        data[selectedProjectIndex].phases.splice( selectedPhaseIndex,0,selectedData);
+        populateProjectsTable();  
+    } 
+}
+
+function updateProjectField(index, campo, value){
+    if (campo === "penalty"){
+        value = Math.round(value);
+        if (value >10){
+            value = 10;
+        }
+        if (value < 0){
+            value = 0;
+        }
+
+    }
+    data[index][campo] = value;
+    populateProjectsTable();
+}
+
+function updatePhaseField(index, campo, value){
+    if (campo === "duration"){
+        value = Math.round(value);
+        if (value >1000){
+            value = 1000;
+        }
+        if (value < 0){
+            value = 0;
+        }
+
+    }
+    if (campo === "start"){
+        value = Math.round(value);
+        if (value >1){
+            value = 1;
+        }
+        if (value < 0){
+            value = 0;
+        }
+        if (index === 0){
+            value = 0;
+        }
+
+    }
+    data[selectedProjectIndex].phases[index][campo] = value;
+    populatePhasesTable();
+}
+
+function updateResourceField(index, campo, value){
+    if (campo === "employed"){
+        value = Math.round(value);
+        if (value >100){
+            value = 100;
+        }
+        if (value < 1){
+            value = 1;
+        }
+    }
+    data[selectedProjectIndex].phases[selectedPhaseIndex].resources[index][campo] = value;
+    populateResourcesTable();
+}
+
+var resourcesList = [];
+var numberOfPhases= 0;
+
+function getInfo(){
+    numberOfPhases= 0;
+    data.forEach((project) => {
+        project.phases.forEach((phase) =>{
+        numberOfPhases++;
+        phase.resources.forEach((resource) =>{
+
+            if (!resourcesList.includes(resource)){
+                resourcesList.push(resource);
+            }
+            });
+    
+        });
+
+    });
+}
+
+function updateChart(days){
+    
+    var tableHead = document.querySelectorAll('#graphic2 thead')[0];
+    tableHead.innerHTML = "";
+    var monthHead = document.createElement('tr');
+    var dayHead = document.createElement('tr');
+    var monthString = "";
+    var dayString = "";
+    var newDay = new Date();
+    var oldMonth = newDay.getMonth();
+    var spanMonth =  0;
+    days.forEach((day, i) => { 
+        if (newDay.getDay()===0){newDay.setDate(newDay.getDate()+1)}
+        if (newDay.getDay()===6){newDay.setDate(newDay.getDate()+2)}
+
+        dayString = dayString + "<th width='20px'>"+newDay.getDate()+"</th>";
+        if (newDay.getMonth()===oldMonth){spanMonth++}
+        else{ 
+            monthString = monthString + "<th width='20px'class='text-center' colspan="+spanMonth+">"+(oldMonth+1)+"</th>";
+
+            
+            spanMonth = 1;
+            oldMonth = newDay.getMonth();
+        }
+        newDay.setDate(newDay.getDate()+1);
+        
+    })
+    if(spanMonth!=0){
+        monthString = monthString + "<th width='25px' class='text-center' colspan="+spanMonth+">"+(oldMonth+1)+"</th>";
+
+    }
+
+    monthHead.innerHTML = monthString;
+    dayHead.innerHTML = dayString ;
+    tableHead.appendChild (monthHead); 
+    tableHead.appendChild(dayHead);
+    
+    var tableBody = document.querySelectorAll('#graphic2 tbody')[0];
+    tableBody.innerHTML = "";
+    var dayString=""
+   
+    for (let i=0; i < days[0].used.length; i++){
+        var rowData = document.createElement('tr');
+        days.forEach(day => {
+            if (day.used[i]===1){dayString = dayString + "<td bgcolor='blue'></td>"}
+            else {dayString = dayString + "<td></td>"}
+        })
+        rowData.innerHTML = dayString
+        tableBody.appendChild(rowData)
+        dayString=""
+    }
+
+
+
+
+
+
+
+    
+    tableHead = document.querySelectorAll('#graphic1 thead')[0];
+    tableHead.innerHTML = "";
+    var rowHead = document.createElement('tr');
+    rowHead.innerHTML="<th></th><th>Month</th>"
+    tableHead.appendChild (rowHead); 
+    rowHead = document.createElement('tr');
+    rowHead.innerHTML="<th>Projects</th><th>Phases/Day</th>"
+    tableHead.appendChild (rowHead); 
+    
+
+    tableBody = document.querySelectorAll('#graphic1 tbody')[0];
+    tableBody.innerHTML = "";
+
+    data.forEach((project) => {            
+        var tableRow = document.createElement('tr');
+        tableRow.innerHTML = "<td rowspan="+(project.phases.length)+">"+project.name+"</td><td>"+project.phases[0].name+"</td>";
+        tableBody.appendChild(tableRow);
+        project.phases.forEach((phase, y) => {
+            if (y!=0){
+                tableRow = document.createElement('tr');
+                bodyString = "<td>"+phase.name+"</td>";
+                tableRow.innerHTML = bodyString;
+                tableBody.appendChild(tableRow);
+            }
+        })
+    })
+    projectDelay = calcProjecDelay(days)
+    console.log(projectDelay)
+    populateDelayTable()
+}
+
+function calcProjecDelay(days){
+    var result = []
+    var phaseIndex = 0
+    var finishDay = 0
+    data.forEach((pro, index )=>{
+        var scadenza = new Date(pro.delivery)
+        phaseIndex = phaseIndex + pro.phases.length
+        finishDay = days.length
+        while (days[finishDay-1].used[phaseIndex-1]===0){
+            finishDay--
+        }
+        
+        result[index] = finishDay-calcDayDate(scadenza)
+     })
+    return result
+}
+
+function calcDayDate(data){
+    var newDay = new Date()
+    var numDay = 0
+    while (newDay <= data){
+        newDay.setDate(newDay.getDate()+1)
+        if (!(newDay.getDay()===0 || newDay.getDay()===6)){
+            numDay++
+        }
+    }
+    
+    return numDay
+}
+
+function populateDelayTable(){
+    var tableBody = document.querySelectorAll('#delay tbody')[0]
+    tableBody.innerHTML = ""
+    var tableRow = null
+    var cost = 0
+    var totalCost = 0
+    data.forEach((project, index) => {            
+        tableRow = document.createElement('tr');
+        if (projectDelay[index] > 0){ cost = projectDelay[index]* project.penalty }
+        tableRow.innerHTML = "<td>"+project.name+"</td><td>"+projectDelay[index]+"</td><td>"+cost+"</td>"
+        totalCost = totalCost + cost
+        tableBody.appendChild(tableRow);
+    })
+    tableRow = document.createElement('tr');
+    tableRow.innerHTML = "<td>Total cost</td><td></td><td>"+totalCost+"</td>";
+    tableBody.appendChild(tableRow);
+}
---
+++ algorithm2.js
@@ -0,0 +1,110 @@
+const PARALLEL = 0
+const SEQUENCE = 1
+
+
+class GANTT{
+
+    constructor(data){
+        this.data = data
+        this.phaseIndex = 0
+        this.numberOfPhases = 0
+        this.setNumberOfPhases()
+
+        this.solution = []//array di giorni
+        this.resources = new Set()
+        this.setResources()
+        
+        this.data.forEach(project => 
+            this.insertProject(project)
+        )
+    }
+    setNumberOfPhases(){
+        data.forEach(project =>{
+            this.numberOfPhases = this.numberOfPhases + project.phases.length
+        })
+    }
+
+    setResources(){
+
+        this.data.forEach(project => project.phases.forEach(phase => phase.resources.forEach(resource => 
+            this.resources.add(resource.name)
+        )))
+    }
+    getNewDay(){
+        const map = new Map()
+        this.resources.forEach(res => map.set(res, 0))
+        var day = {used: [], resource: map}
+        console.log(this.numberOfPhases)
+        for (let i=0; i<this.numberOfPhases; i++){
+            day.used.push(0);   
+        }
+        return day
+    }
+    getGantt(){
+        console.log(this.solution)
+        return this.solution
+    }
+
+
+    insertProject(project){
+        var currentDay = 0
+        project.phases.forEach((phase, index) => {
+            
+            
+            if (index!=0){ //cerca il giorno in cui cominciare a inserire la fase se PARALLEL
+                if (phase.start===PARALLEL){
+                    currentDay = 0
+
+                    while (this.solution[currentDay].used[this.phaseIndex-1]===0){currentDay++}
+                }
+            }
+            for(let i = 0; i < phase.duration; i++){0
+                
+                do{
+                var isFree = true
+                var emp = 0
+                if (this.solution.length <= currentDay){
+                    isFree = true
+                    var newDay = this.getNewDay()
+                    newDay.used[this.phaseIndex] = 1
+                    phase.resources.forEach(res => {
+                        newDay.resource.set(res.name, res.employed)
+
+                    })
+                    newDay.used[this.phaseIndex] = 1
+                    this.solution[currentDay] = newDay
+                }
+                else{
+                    phase.resources.forEach(res => {
+                        emp =this.solution[currentDay].resource.get(res.name)
+                        if ((res.employed + emp) > 100){
+                            isFree = false 
+                            currentDay++ 
+                        }
+                    })
+                    if  (isFree){
+                        phase.resources.forEach(res => {
+                            emp =this.solution[currentDay].resource.get(res.name)
+
+                            this.solution[currentDay].resource.set(res.name, (res.employed + emp))
+                        })
+                        this.solution[currentDay].used[this.phaseIndex]=1
+                    }   
+                    } 
+
+                
+                    
+                  
+
+                }
+                while (!isFree)    
+                currentDay++
+            }
+
+            this.phaseIndex++
+            
+
+                
+        })    
+   } 
+}
